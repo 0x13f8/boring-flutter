04dd4a65fed2a86b7fcd1e71d9e0f662015c5197
null <->
2021-09-20 00:20:48 +0700

    Initial commit

---
+++ template_frida_hook_ios.js
@@ -0,0 +1,27 @@
+function hook_ssl_verify_result(address)
+{
+    Interceptor.attach(address, {
+        onEnter: function(args) {
+            console.log("onEnter: Disabling SSL validation");
+        },
+        onLeave: function(retval)
+        {
+            console.log("onLeave: Disabling SSL validation");
+            retval.replace(0x1);
+        }
+    });
+}
+
+function disablePinning()
+{
+    console.log("Enter disablePinning()");
+    var m = Process.findModuleByName("Flutter");     // formerly known as "Runner"
+
+    hook_ssl_verify_result(m.base.add(0x00000000))
+}
+
+if (ObjC.available) {
+    disablePinning()
+} else {
+    send("error: Objective-C Runtime is not available!");
+}
